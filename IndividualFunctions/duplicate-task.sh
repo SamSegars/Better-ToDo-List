0254f1920ae683d1e239774d408dd870f814631e
Sam Segars <sam.segars@yahoo.com>
2021-10-18 16:32:58 -0400

    Increased consitency in style on duplicate.

--- IndividualFunctions/duplicate-task.sh
+++ IndividualFunctions/duplicate-task.sh
@@ -9,13 +9,13 @@ if test "$Task" = "" ; then
 fi
 
   read -p "Are you sure you want to duplicate $Task. $FullTask? [y,N]" Confirm
-  if test "$Confirm" = y -o "$Confirm" = Y ; then
+  if test "$Confirm" = "y" -o "$Confirm" = "Y" ; then
     echo $FullTask >> ~/.todo/.list
     NTask=$(cat -n ~/.todo/.list |tail -n 1)
     echo "Created "$NTask""
     exit
   fi
-  if test "$Confirm" = n -o "$Confirm" = N -o "$Confirm" = "" ; then
+  if test "$Confirm" = "n" -o "$Confirm" = "N" -o "$Confirm" = "" ; then
         echo "Canceled"
         exit
   else
