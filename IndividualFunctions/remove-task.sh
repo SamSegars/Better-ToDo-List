df5803d846b0dade98159234225ef69d037db975
Sam Segars <sam.segars@yahoo.com>
2021-10-18 16:25:52 -0400

    Removed shopt statement, and increased consistency across remove-task.

--- IndividualFunctions/remove-task.sh
+++ IndividualFunctions/remove-task.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 Continue=y
-shopt -s nocasematch
+
 while test "$Continue" = "Y" -o "$Continue" = "y"; do
 read -p "What task # would you like to remove?" Task
 FullTask=$(sed -n "$Task"p ~/.todo/.list)
@@ -11,14 +11,14 @@ if test "$Task" = "" ; then
 fi
 
   read -p "Are you sure you want to remove $Task. $FullTask? [y,N]" Confirm
-  if test "$Confirm" = "y" -o $Confirm = "Y" ; then
+  if test "$Confirm" = "y" -o "$Confirm" = "Y" ; then
   sed -i ""$Task"d" ~/.todo/.list
   echo "Removed $Task. $FullTask from task list"
   read -p "Do you want to continue? [y,N]" Continue
   fi
 
   if test "$Confirm" != "y" -a "$Confirm" != "Y" ; then
-    if test "$Confirm" = n -o "$Confirm" = N -o "$Confirm" = "" ; then
+    if test "$Confirm" = "n" -o "$Confirm" = "N" -o "$Confirm" = "" ; then
           echo "Canceled"
           read -p "Do you want to continue? [y,N]" Continue
     else
