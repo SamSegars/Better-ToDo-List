72840d70d5d417271bef35aa41f34caf65b8828d
Sam Segars <sam.segars@yahoo.com>
2021-10-18 11:10:57 -0400

    Changed from using a file to hold the next number to utilizing the cat -n argument.

--- add-list.sh
+++ add-list.sh
@@ -1,19 +1,13 @@
-#!/bin/bash 
+#!/bin/bash
 directory=~/.todo
-number=$directory/.number
 list=$directory/.list
 if [ ! -d "$directory" ]; then
   mkdir $directory
-  touch $number
-  echo "0" > $number
   touch $list
 fi
 
 read -p "What task would you like to add?" Task
 
-ON=$(cat $number)
-NN=$(($ON + 1))
-echo $NN > $number
-
-echo "$NN.$Task" >> $list
-echo "$NN.$Task Succesfully added"
+echo "$Task" >> $list
+NTask=$(cat -n ~/.todo/.list | tail -n 1)
+echo ""$NTask" Succesfully added"
