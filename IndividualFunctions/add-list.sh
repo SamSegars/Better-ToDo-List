1605d569365a2316488d7a2b3c29ebc0bb3abb61
Sam Segars <sam.segars@yahoo.com>
2021-10-18 14:34:57 -0400

    removed variables for optimization purposes.

--- IndividualFunctions/add-list.sh
+++ IndividualFunctions/add-list.sh
@@ -1,13 +1,16 @@
-#!/bin/bash
-directory=~/.todo
-list=$directory/.list
-if [ ! -d "$directory" ]; then
-  mkdir $directory
-  touch $list
+#!/bin/bash -x
+if [ ! -d ~/.todo ]; then
+  mkdir ~/.todo
+  touch ~/.todo/.list
 fi
 
 read -p "What task would you like to add?" Task
 
-echo "$Task" >> $list
+if test "$Task" = "" ; then
+  echo "No Input recorded, Exiting..."
+  exit 1
+fi
+
+echo "$Task" >> ~/.todo/.list
 NTask=$(cat -n ~/.todo/.list | tail -n 1)
 echo ""$NTask" Succesfully added"
