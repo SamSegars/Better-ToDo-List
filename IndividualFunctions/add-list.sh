6565247d572227cfcb19782d87379af1b719656c
Sam Segars <sam.segars@yahoo.com>
2021-10-18 16:05:23 -0400

    Added a loop to support adding multiple things without having to exit.

--- IndividualFunctions/add-list.sh
+++ IndividualFunctions/add-list.sh
@@ -3,7 +3,8 @@ if [ ! -d ~/.todo ]; then
   mkdir ~/.todo
   touch ~/.todo/.list
 fi
-
+Continue=y
+while test "$Continue" = "y" -o "$Continue" = "Y"; do
 read -p "What task would you like to add?" Task
 
 if test "$Task" = "" ; then
@@ -14,3 +15,5 @@ fi
 echo "$Task" >> ~/.todo/.list
 NTask=$(cat -n ~/.todo/.list | tail -n 1)
 echo ""$NTask" Succesfully added"
+read -p "Would you like to continue? [y,N]" Continue
+done
