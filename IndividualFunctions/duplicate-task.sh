c8c9b2b7cc7285a2f4c62638c82cd343080507b1
Sam Segars <sam.segars@yahoo.com>
2021-10-18 16:29:05 -0400

    removed confirmno function as it no longer had a purpose.

--- IndividualFunctions/duplicate-task.sh
+++ IndividualFunctions/duplicate-task.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-ConfirmNo(){
-  if test "$Confirm" = n -o "$Confirm" = N -o "$Confirm" = "" ; then
-        echo "Canceled"
-        exit
-  else
-        echo "Invalid Answer"
-        exit
-  fi
-}
-
 read -p "Which task # would you like to duplicate?" Task
 FullTask=$(sed -n "$Task"p ~/.todo/.list)
 
@@ -20,10 +10,15 @@ fi
 
   read -p "Are you sure you want to duplicate $Task. $FullTask? [y,N]" Confirm
   if test "$Confirm" = y -o "$Confirm" = Y ; then
-  echo $FullTask >> ~/.todo/.list
-  NTask=$(cat -n ~/.todo/.list |tail -n 1)
-  echo "Created "$NTask""
-  exit
+    echo $FullTask >> ~/.todo/.list
+    NTask=$(cat -n ~/.todo/.list |tail -n 1)
+    echo "Created "$NTask""
+    exit
   fi
-  ConfirmNo
-  exit
+  if test "$Confirm" = n -o "$Confirm" = N -o "$Confirm" = "" ; then
+        echo "Canceled"
+        exit
+  else
+        echo "Invalid Answer"
+        exit
+  fi
